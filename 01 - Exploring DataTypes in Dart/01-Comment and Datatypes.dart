8478258e8caee51ad00539f0a14d0f05ba2c6095
Om Joshi <58983530+OSJ26@users.noreply.github.com>
2023-06-15 08:58:47 +0530

    Dart fundamentals

---
+++ 01 - Exploring DataTypes in Dart/01-Comment and Datatypes.dart
@@ -0,0 +1,40 @@
+void main() {
+  print("This is simple demo");
+
+  //Here is the single line comment
+
+  /*
+   * this is multiline demo 
+   * */
+
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+
+  //DataTypes in dart
+  /*
+   * Number Data Type
+   *  -> Int
+   *  -> Double
+   * -> string
+   * -> boolean
+   * -> list (known as array)
+   * -> Maps
+   * -> Runes (For character string)
+   * -> symbols
+   * 
+   * -> All the data type in Dart is object 
+   * */
+
+  //Numbers : int
+  int age = 10;
+
+  //Numbers : Double
+  double age1 = 10.20;
+
+  //strings
+  String name = "osj";
+
+  //boolean
+  bool isValid = true;
+}
